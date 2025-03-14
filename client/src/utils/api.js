2f00d21d97e365361ce5ba3dad0a6ca6060356e4
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-14 17:24:34 +0100

    Create utils/api.js for easy axios instance

---
+++ client/src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  withCredentials: true, // for cookies
+});
+
+export default api;
