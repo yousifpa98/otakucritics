369a232d8d51a500ef699067db4ba0392738c0f5
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-14 17:17:48 +0100

    add /me and /logout route to userRouter

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { createUser, login } from "../controllers/userController.js";
+import {
+  createUser,
+  currentUser,
+  login,
+  logout,
+} from "../controllers/userController.js";
+import { authenticate } from "../middleware/jwt.js";
 
 const router = Router();
 
 router.post("/register", createUser);
 router.post("/login", login);
+router.get("/me", authenticate, currentUser);
+router.post("/logout", logout);
 
 export default router;
