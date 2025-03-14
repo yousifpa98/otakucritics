116665e7c43907a91991bb5a6f7e3be4f95f5e26
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-14 18:07:45 +0100

    add main around App component and min height 100vh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Register from "./Components/Register";
 import { useAuth } from "./contexts/AuthContext";
+import Hero from "./Views/Hero/Hero";
+import "./App.css";
 
 const App = () => {
   const user = useAuth();
 
   return (
-    <Routes>
-      <Route path="/" element={<div>App</div>} />
-      <Route 
-        path="/register" 
-        element={user ? <Navigate to="/" /> : <Register />} 
-      />
-    </Routes>
+    <main>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+      </Routes>
+    </main>
   );
 };
 
