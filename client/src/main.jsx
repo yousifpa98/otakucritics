743fd38d5ab407545e2e4b0267226dc7b6b8bbe5
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-14 17:25:26 +0100

    change order of context providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import Layout from "./Layout/Layout.jsx";
+import "./fonts.css";
+import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <BrowserRouter>
+      <ThemeProvider>
+        <AuthProvider>
+          <Layout>
+            <App />
+          </Layout>
+        </AuthProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 );
