63389d159562905fb38de12ed0ebaf3f4eea244e
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-14 17:36:08 +0100

    FEAT: Nav component with home and toplists link

--- client/src/Components/MobileNav.jsx
+++ client/src/Components/MobileNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 const MobileNav = () => {
-  return (
-    <div>MobileNav</div>
-  )
-}
+  const navItems = [
+    { id: "23t09i", name: "Home", to: "/" },
+    { id: "98f4k2", name: "TopLists", to: "/toplists" },
+  ];
 
-export default MobileNav
+  return (
+    <nav>
+      <ul className="nav-list">
+        {navItems.map((nI) => {
+          return (
+            <li className="nav-item">
+              <Link to={nI.to}>{nI.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default MobileNav;
