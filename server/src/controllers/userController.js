c0afeddb79b761eebd00a6690eb9bf7c4f54d6f0
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-17 23:31:27 +0100

    minor bugfixing

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -115,3 +115,70 @@ export const logout = (req, res) => {
     });
   }
 };
+
+// Add to watchlist
+export const addToWatchlist = async (req, res) => {
+  try {
+    const { animeId } = req.body;
+    const userId = req.user.userId;
+
+    // Check if animeId is provided
+    if (!animeId) {
+      return res.status(400).json({ message: "Anime ID is required." });
+    }
+
+    // Find user by ID
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Check if anime is already in watchlist
+    if (user.watchlist.includes(animeId)) {
+      return res
+        .status(409)
+        .json({ message: "Anime is already in watchlist." });
+    }
+
+    // Add anime to watchlist
+    user.watchlist.push(animeId);
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "Anime added to watchlist", watchlist: user.watchlist });
+  } catch (error) {
+    console.error("Error adding to watchlist:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// delete anime from watchlist
+export const removeFromWatchlist = async (req, res) => {
+  try {
+    const { animeId } = req.body;
+    const userId = req.user.userId;
+
+    if (!animeId) {
+      return res.status(400).json({ message: "Anime ID is required" });
+    }
+
+    const user = await User.findOne(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // check for anime on watchlist
+    if (!user.watchlist.includes(animeId))
+      return res.status(404).json({ message: "Anime not in watchlist." });
+
+    user.watchlist = user.watchlist.filter((id) => id !== animeId);
+    await user.save();
+
+    res.status(200).json({ message: "Anime removed from watchlist." });
+  } catch (error) {
+    console.error("Error removing from watchlist:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
