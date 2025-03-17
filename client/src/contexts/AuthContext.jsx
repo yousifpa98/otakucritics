bd3ebc6b6f2b6f42e42c9870f0bcb9529806901a
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-17 23:28:18 +0100

    add setUser to authContext value

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -43,7 +43,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading }}>
+    <AuthContext.Provider value={{ user, setUser, login, logout, loading }}>
       {children}
     </AuthContext.Provider>
   );
