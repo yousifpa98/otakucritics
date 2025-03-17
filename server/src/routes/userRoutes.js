8b762b5f9de1c8fb998cb9e9c46890ed24569f53
yousifpa98 <yousif.paulus@dci-student.org>
2025-03-17 23:32:06 +0100

    add watchlist routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
+  addToWatchlist,
   createUser,
   currentUser,
   login,
   logout,
+  removeFromWatchlist,
 } from "../controllers/userController.js";
 import { authenticate } from "../middleware/jwt.js";
 
@@ -13,5 +15,7 @@ router.post("/register", createUser);
 router.post("/login", login);
 router.get("/me", authenticate, currentUser);
 router.post("/logout", logout);
+router.post("/watchlist", authenticate, addToWatchlist);
+router.delete("/watchlist", authenticate, removeFromWatchlist);
 
 export default router;
